bfd2bfcf115189e4c8cbb35f70a3a37c491b49c2
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-01 21:00:16 +0000

    se agrego los metodos constructores de gasto global y mensual

--- trunk/TpFinal/src/gastos/GastoGlobal.java
+++ trunk/TpFinal/src/gastos/GastoGlobal.java
@@ -2,11 +2,9 @@ package gastos;
 
 public class GastoGlobal extends Gasto {
 	
-	public GastoGlobal(float fijo, float porcentual){
-		
-		this.setFijo(fijo);
-		this.setNombre("Global");
-		this.setPorcentual(porcentual/100);
+	public GastoGlobal(float monto, TipoDeGasto tipoDeGasto){
+		this.setMonto(monto);
+		this.setTipoDeGasto(tipoDeGasto);
 		
 	}
 
