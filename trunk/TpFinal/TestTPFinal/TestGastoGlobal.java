926ab12cc0d921e1ca01d1139e00c967ab6bddd6
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-03 23:38:06 +0000

    veo lo del test pero no sale jaja

---
+++ trunk/TpFinal/TestTPFinal/TestGastoGlobal.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import gastos.Fijo;
+import gastos.GastoGlobal;
+import gastos.Porcentual;
+import gastos.TipoDeGasto;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import prestamos.Prestamo;
+
+
+public class TestGastoGlobal {
+  
+	private GastoGlobal gastoGlobalFijo;
+	private GastoGlobal gastoGlobalPorcentual;
+	
+	@Before 
+	public void setUp(){
+		gastoGlobalFijo = new GastoGlobal(new Fijo(8));
+		gastoGlobalPorcentual = new GastoGlobal(new Porcentual(10));
+	}
+	
+	@Test
+	public void testCalcularGasto() {
+		Prestamo mockedPrestamo = mock(Prestamo.class);
+		//when(mockedPrestamo.getMontoTotal()).thenReturn((double) 25000); 
+		gastoGlobalFijo.calcularGasto(mockedPrestamo);
+		verify(mockedPrestamo).setMontoTotal(anyDouble());
+		
+		//fail("Not yet implemented");
+	}
+
+}
