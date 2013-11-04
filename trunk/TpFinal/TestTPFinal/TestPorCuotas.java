ed4d9439ab8986eb90db50cadf4248e0038e35b9
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-04 15:37:27 +0000

    se agrego un solo test para PorCuotas con mock

---
+++ trunk/TpFinal/TestTPFinal/TestPorCuotas.java
@@ -0,0 +1,46 @@
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import prestamos.Prestamo;
+import busqueda.PorCuotas;
+
+public class TestPorCuotas {
+	
+	private PorCuotas porCuotas;
+	private static final Integer desde = 1;
+	private static final Integer hasta = 15;
+	public Integer retorno = 12;
+
+	@Before
+	public void setUp(){
+		porCuotas = new PorCuotas(desde,hasta);
+
+	}
+	
+
+	@Test
+	public void testConstructor() {
+		assertNotNull(porCuotas);
+		assertEquals("Se fija si el desde es igual",desde, porCuotas.getDesde());
+		assertEquals("Se fija si el hasta es igual",hasta, porCuotas.getHasta());
+	}
+
+	@Test
+	public void testRespetaCondicion(){
+		Prestamo mockedPrestamo = mock(Prestamo.class);
+		
+		when(mockedPrestamo.cantidadDeCuotas()).thenReturn(retorno);
+		assertTrue(porCuotas.respetaCondicion(mockedPrestamo));
+		
+		when(mockedPrestamo.cantidadDeCuotas()).thenReturn(retorno+4);
+		assertFalse(porCuotas.respetaCondicion(mockedPrestamo));
+		
+		
+	}
+
+}
--- trunk/TpFinal/TestTPFinal/TestTipoDeGastoPorcentual.java
+++ trunk/TpFinal/TestTPFinal/TestTipoDeGastoPorcentual.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class testTipoDeGastoPorcentual {
+public class TestTipoDeGastoPorcentual {
 
 	private TipoDeGasto porcentual;
 	private double numeroAux;
