312a3a93454dbf0876fc0a837658340e34e2a217
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-04 17:59:42 +0000

    los metodos abstractos terminan en ; no en {}

--- TpFinal/src/estadoPrestamos/EstadoPrestamo.java
+++ TpFinal/src/estadoPrestamos/EstadoPrestamo.java
@@ -4,15 +4,15 @@ import prestamos.Prestamo;
 
 public abstract class EstadoPrestamo {
 	
-	public abstract void seAprobo(Prestamo p){}
+	public abstract void seAprobo(Prestamo p);
 	
-	public abstract void noSeAprobo(Prestamo p){}
+	public abstract void noSeAprobo(Prestamo p);
 	
-	public abstract void sePago(Prestamo p){}
+	public abstract void sePago(Prestamo p);
 	
-	public abstract void noSePago(Prestamo p){}
+	public abstract void noSePago(Prestamo p);
 	
-	public abstract void seFinalizo(Prestamo p){}
+	public abstract void seFinalizo(Prestamo p);
 	
-	public abstract void aDeudorIncobrable(Prestamo p){}
+	public abstract void aDeudorIncobrable(Prestamo p);
 }
