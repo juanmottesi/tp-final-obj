5998dcd33f3673ef77e367e0fd52b6b39d10f80e
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-04 13:59:02 +0000

    se agrego el test de gasto fijo, gasto porcentual y los dos funcionan bien

---
+++ trunk/TpFinal/TestTPFinal/testTipoDeGastoFijo.java
@@ -0,0 +1,35 @@
+import static org.junit.Assert.*;
+import gastos.Fijo;
+import gastos.TipoDeGasto;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class testTipoDeGastoFijo {
+
+	private TipoDeGasto fijo;
+	private double numeroAux;
+	
+	@Before
+	public void setUp(){
+		numeroAux = 10;
+		fijo = new Fijo(numeroAux);
+	}
+	
+	@Test
+	public void testConstructor(){
+		assertNotNull(fijo);
+		assertNotEquals((numeroAux/100), fijo.getMonto(), 0);
+		assertEquals(numeroAux, fijo.getMonto(),0);
+		
+	}
+	
+	
+	@Test
+	public void testCalcularGasto() {
+		assertEquals(numeroAux,fijo.calcularGasto(10000),0);
+		assertEquals(numeroAux,fijo.calcularGasto(0),0);
+	}
+
+}
--- trunk/TpFinal/src/gastos/Fijo.java
+++ trunk/TpFinal/src/gastos/Fijo.java
@@ -5,8 +5,14 @@ public class Fijo extends TipoDeGasto {
 	public Fijo(double monto){
 		this.setMonto(monto);
 	}
-
+	
+	
 	@Override
+	/**
+	 * CalcularGasto es una funcion que retorna el valor que tiene guardado
+	 * en su variable privada.
+	 * El paramatro montoAAplicarGastos no se usa.
+	 */
 	public double calcularGasto(double montoAAplicarGastos) {
 		
 		return this.getMonto();
