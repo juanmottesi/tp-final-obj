445fd70f5c16e27909ab2ed8d8c3261989958ecb
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-05 13:46:40 +0000

    se borra parametro sin uso en la clase testPorOr. se agrega el TestGastoGlobal funcional.

--- trunk/TpFinal/TestTPFinal/gastos/TestGastoGlobal.java
+++ trunk/TpFinal/TestTPFinal/gastos/TestGastoGlobal.java
@@ -8,29 +8,85 @@ import gastos.TipoDeGasto;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import prestamos.Prestamo;
 
 
 public class TestGastoGlobal {
   
-	private GastoGlobal gastoGlobalFijo;
-	private GastoGlobal gastoGlobalPorcentual;
+	private GastoGlobal gastoGlobal;
 	
-	@Before 
-	public void setUp(){
-		gastoGlobalFijo = new GastoGlobal(new Fijo(8));
-		gastoGlobalPorcentual = new GastoGlobal(new Porcentual(10));
+	
+	@Mock
+	TipoDeGasto mockedTipoDeGasto = mock(TipoDeGasto.class);
+	
+	public void initializeFijo(){
+		mockedTipoDeGasto = mock(Fijo.class);
+		gastoGlobal = new GastoGlobal(mockedTipoDeGasto);
+	}
+
+	public void initializePorcentual(){
+		mockedTipoDeGasto = mock(Porcentual.class);
+		gastoGlobal = new GastoGlobal(mockedTipoDeGasto);
+	}
+	
+	
+	@Test
+	public void testConstructor(){
+		this.initializeFijo();
+		assertNotNull(gastoGlobal);
+		double monto = 25;
+		when(mockedTipoDeGasto.getMonto()).thenReturn(monto);
+		assertEquals(monto, gastoGlobal.getTipoDeGasto().getMonto(),0);
+		
+		this.initializePorcentual();
+		assertNotNull(gastoGlobal);
+		monto = 5;
+		when(mockedTipoDeGasto.getMonto()).thenReturn(monto);
+		assertEquals(monto, gastoGlobal.getTipoDeGasto().getMonto(),0);
+	
 	}
 	
 	@Test
 	public void testCalcularGasto() {
-		Prestamo mockedPrestamo = mock(Prestamo.class);
-		//when(mockedPrestamo.getMontoTotal()).thenReturn((double) 25000); 
-		gastoGlobalFijo.calcularGasto(mockedPrestamo);
-		verify(mockedPrestamo).setMontoTotal(anyDouble());
+		this.initializeFijo();
 		
-		//fail("Not yet implemented");
+		Prestamo mockedPrestamo = mock(Prestamo.class);
+		
+		//seteo el monto del mockedTipoDeGasto en 25.
+		when(mockedTipoDeGasto.getMonto()).thenReturn((double)25);
+		//creo un auxilir para saber corroborar que seteo el monto.
+		double auxiliar = gastoGlobal.getTipoDeGasto().getMonto();
+		//seteo el montoTotal del prestamo en 25000.
+		when(mockedPrestamo.getMontoTotal()).thenReturn((double) 25000);
+		//creo el resultado del calcularGasto del tipoDeGasto.
+		when(mockedTipoDeGasto.calcularGasto(25000)).thenReturn(auxiliar);
+		//Llamo al metodo a testear
+		gastoGlobal.calcularGasto(mockedPrestamo);
+		//corroboro que el metodo tenga que devolver 25000 - 25 = 24975.
+		verify(mockedPrestamo).actualizarGastos(24975);
+		
+		
+		this.initializePorcentual();
+		
+		//seteo el monto del mockedTipoDeGasto en 10%.
+		when(mockedTipoDeGasto.getMonto()).thenReturn((double)10/100);
+		
+		//creo un auxilir para saber corroborar que seteo el monto.
+		auxiliar = gastoGlobal.getTipoDeGasto().getMonto();
+		
+		//seteo el montoTotal del prestamo en 25000.
+		when(mockedPrestamo.getMontoTotal()).thenReturn((double) 25000);
+		
+		//creo el resultado del calcularGasto del tipoDeGasto.
+		when(mockedTipoDeGasto.calcularGasto(25000)).thenReturn(25000* auxiliar);
+		
+		//Llamo al metodo a testear
+		gastoGlobal.calcularGasto(mockedPrestamo);
+		//corroboro que el metodo tenga que devolver 25000 * 10%(2500) = 22500.
+		verify(mockedPrestamo).actualizarGastos(22500);
+		
 	}
 
 }
