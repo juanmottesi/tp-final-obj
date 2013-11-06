232b919ad62231656adc330ff2894dd499cf97f1
rodrigo.ariel.gonzalez06@gmail.com <rodrigo.ariel.gonzalez06@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-06 17:53:28 +0000

    Se hizo el método finalizarPrestamo() en la clase Prestamo!!!

--- trunk/TpFinal/src/estadoPrestamos/Finalizado.java
+++ trunk/TpFinal/src/estadoPrestamos/Finalizado.java
@@ -5,19 +5,19 @@ import prestamos.Prestamo;
 public class Finalizado extends EstadoPrestamo {
 	
 	public void seAprobo(Prestamo p){}
+		//Levantar una excepcion!!!
 	
 	public void noSeAprobo(Prestamo p){}
+		//Levantar una excepcion!!!
 	
 	public void sePago(Prestamo p){}
+		//Levantar una excepcion!!!
 	
 	public void noSePago(Prestamo p){}
+		//Levantar una excepcion!!!
 	
 	public void seFinalizo(Prestamo p){
-		
-		if(p.estanTodasLasCuotasPagas()){
-			
-			p.setEstado(new Finalizado());
-	}
+		//Levantar una excepcion!!!
 }
 	
 	public void aDeudorIncobrable(Prestamo p){}
