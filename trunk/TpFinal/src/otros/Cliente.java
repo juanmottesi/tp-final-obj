35471402114ab28853abbd09b38e03bc7b7e4db1
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-06 18:55:29 +0000

    se agrega constructor y un metodo

--- trunk/TpFinal/src/otros/Cliente.java
+++ trunk/TpFinal/src/otros/Cliente.java
@@ -1,13 +1,30 @@
 package otros;
 
+import java.util.List;
+import java.util.Vector;
+
+import estadoPrestamos.DeudorIncobrable;
+import estadoPrestamos.EnDeuda;
+import estadoPrestamos.Solicitado;
+import prestamos.Prestamo;
+
 public class Cliente {
 	
 	private String apellido;
 	private Integer dni;
 	private String direccion;
 	private String nombre;
+	private List<Prestamo> prestamos;
 	
 	
+	public List<Prestamo> getPrestamos() {
+		return prestamos;
+	}
+
+	public void setPrestamos(List<Prestamo> prestamos) {
+		this.prestamos = prestamos;
+	}
+
 	public String getNombre() {
 		return nombre;
 	}
@@ -40,6 +57,42 @@ public class Cliente {
 		this.direccion = direccion;
 	}
 	
+	public Cliente(String apellido, String nombre, String direccion, Integer documento){
+		this.setApellido(apellido);
+		this.setNombre(nombre);
+		this.setDireccion(direccion);
+		this.setDni(documento);	
+		this.setPrestamos(new Vector<Prestamo>());
+	}
 	
-
+	public boolean puedoAgregarPrestamo()throws AgregarPrestamoAClienteException{
+		int contarPrestamos = 0;
+		for(Prestamo p : this.getPrestamos()){
+			if(contarPrestamos == 2){
+				if(p.getEstado().equals(new Solicitado()) || p.getEstado().equals(new EnDeuda()) ){
+					contarPrestamos+=1;
+				}
+				if(p.getEstado().equals(new DeudorIncobrable())) throw new AgregarPrestamoAClienteException{
+					return false;
+				}
+			}
+			else{
+				return false;
+			}
+		
+		}
+		return true;
+	}
+	
+	public void agregarPrestamo(Prestamo prestamo){
+		try{
+			if(this.puedoAgregarPrestamo()){
+				this.getPrestamos().add(prestamo);
+			}
+			
+		}
+		catch(AgregarPrestamoAClienteException e){
+			System.out.println(e);
+		}
+	}
 }
