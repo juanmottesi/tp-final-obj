d8b4418b8d9b02e5dba2862f1f7f350f673cba95
rodrigo.ariel.gonzalez06@gmail.com <rodrigo.ariel.gonzalez06@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-06 17:30:47 +0000

    Se finalizó el comportamiento de la la clase solicitado!!!

--- TpFinal/src/estadoPrestamos/Solicitado.java
+++ TpFinal/src/estadoPrestamos/Solicitado.java
@@ -10,15 +10,25 @@ public class Solicitado extends EstadoPrestamo {
 		p.setEstado ( new EnCurso());
 	}
 	
-	public void noSeAprobo(Prestamo p){}
+	public void noSeAprobo(Prestamo p){
+		
+		p.setEstado(new Rechazado());
+	}
 	
 	public void sePago(Prestamo p){}
+		//Levantar una excepcion!!!
 	
 	public void noSePago(Prestamo p){}
+		//Levantar una excepcion!!!
 	
-	public void seFinalizo(Prestamo p){}
+	public void seFinalizo(Prestamo p){
+		//Excepcion no puedo finalizar algo que no tengo ni aprobado
+	}
 	
-	public void aDeudorIncobrable(Prestamo p){}
+	public void aDeudorIncobrable(Prestamo p){
+		
+		p.setEstado(new Rechazado());
+	}
 
 
 
