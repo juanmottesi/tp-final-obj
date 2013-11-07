88a8a5214c6d237a4f124ea24eb83bf24414acc5
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-07 14:57:53 +0000

    se modifico el tem

--- trunk/TpFinal/src/otros/CalculoValorCuota.java
+++ trunk/TpFinal/src/otros/CalculoValorCuota.java
@@ -6,6 +6,10 @@ import installment.calculator.model.AdvanceModeInstallment;
 
 public class CalculoValorCuota {
 
+	public CalculoValorCuota(){
+		super();
+	}
+	
 	public static double calcularCuota(double montoTotal, Integer temCorrespondiente, Integer cantCuotas) throws InstallmentCountException, InvalidAmountException{
 		//(double amount, double interest, int installmentCount);
 		return AdvanceModeInstallment.calculateInstallmentValue(montoTotal, temCorrespondiente, cantCuotas);
