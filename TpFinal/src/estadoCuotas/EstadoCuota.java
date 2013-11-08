9e54056a437531ff5676bc835e1701e566868204
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-08 16:58:51 +0000

    se dejo como estaba el equals de estado cuotas

--- TpFinal/src/estadoCuotas/EstadoCuota.java
+++ TpFinal/src/estadoCuotas/EstadoCuota.java
@@ -9,18 +9,27 @@ public abstract class EstadoCuota {
 	public abstract void aVencido(Cuota cuota)throws EstadoCuotaException;
 	
 
-	public boolean equals(EstadoCuota estadoCuota){
-		if(estadoCuota == null){
-			return false;
-		}
-		if(this == estadoCuota){
-			return true;
-		}	
-		if(this.getClass() != estadoCuota.getClass()){
-			return false;
-		}
-		
-		return true;
-	}
-	
+//	public boolean equals(EstadoCuota estadoCuota){
+//		if(estadoCuota == null){
+//			return false;
+//		}
+//		if(this == estadoCuota){
+//			return true;
+//		}	
+//		if(this.getClass() != estadoCuota.getClass()){
+//			return false;
+//		}
+//		
+//		return true;
+//	}
+	@Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+       return true;
+	}	
 }
