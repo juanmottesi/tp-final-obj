cd422f7ece920854663830577f7e115eed88df8b
rodrigo.ariel.gonzalez06@gmail.com <rodrigo.ariel.gonzalez06@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-09 04:58:06 +0000

    Test a Deudor Incobrable finalizado

---
+++ TpFinal/TestTPFinal/estadoPrestamo/TestDeudorIncobrable.java
@@ -0,0 +1,71 @@
+package estadoPrestamo;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import prestamos.Prestamo;
+import estadoPrestamos.DeudorIncobrable;
+import estadoPrestamos.EstadoPrestamo;
+import exceptions.AprobadoException;
+import exceptions.DeudorIncobrableException;
+import exceptions.FinalizadoException;
+import exceptions.RechazadoException;
+import static org.mockito.Mock.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TestDeudorIncobrable {
+	
+	private DeudorIncobrable enDeudorIncobrable;
+	private Prestamo mockedPrestamo;
+	
+	@Before
+	public void setUp() throws Exception {
+		enDeudorIncobrable= new DeudorIncobrable();
+		mockedPrestamo= mock(Prestamo.class);
+		
+	}
+	
+	@Test(expected = DeudorIncobrableException.class)
+	public void testADeudorIncobrable() throws DeudorIncobrableException {
+		
+		when(mockedPrestamo.getEstado()).thenReturn(enDeudorIncobrable);
+		
+		mockedPrestamo.getEstado().aDeudorIncobrable(mockedPrestamo);
+		
+		assertSame(enDeudorIncobrable, mockedPrestamo.getEstado());
+		}
+	
+	@Test(expected = AprobadoException.class)
+	public void testAprobar() throws AprobadoException {
+		
+		when(mockedPrestamo.getEstado()).thenReturn(enDeudorIncobrable);
+		
+		mockedPrestamo.getEstado().aprobar(mockedPrestamo);
+		
+		assertSame(enDeudorIncobrable, mockedPrestamo.getEstado());
+		}
+
+	@Test(expected = RechazadoException.class)
+	public void testRechazar() throws RechazadoException {
+		
+		when(mockedPrestamo.getEstado()).thenReturn(enDeudorIncobrable);
+		
+		mockedPrestamo.getEstado().rechazar(mockedPrestamo);
+		
+		assertSame(enDeudorIncobrable, mockedPrestamo.getEstado());
+		}
+
+	@Test (expected = FinalizadoException.class)
+	public void testFinalizar() throws FinalizadoException{
+		
+		when(mockedPrestamo.getEstado()).thenReturn(enDeudorIncobrable);
+		
+		mockedPrestamo.getEstado().finalizar(mockedPrestamo);
+	}
+}
+
+
