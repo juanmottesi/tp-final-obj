00c142c791cc5a05f391bce3cb1e86921a48ba5e
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-15 14:26:18 +0000

    cambie de lugar el estado prestamo xq me confundi de paquete

--- trunk/TpFinal/TestTPFinal/estadoPrestamo/TestEstadoPrestamo.java
+++ trunk/TpFinal/TestTPFinal/estadoPrestamo/TestEstadoPrestamo.java
@@ -15,6 +15,69 @@ import estadoPrestamos.Solicitado;
 
 public class TestEstadoPrestamo {
 
+	private EstadoPrestamo estadoPrestamo1;
+	private EstadoPrestamo estadoPrestamo2;
 	
+	@Before 
+	public void setUp(){
+		estadoPrestamo1 = new EnCurso();
+		estadoPrestamo2 = new EnCurso();
+	}
+	
+	@Test
+	public void testEqualsVerdadero() {
+		assertTrue(estadoPrestamo1.equals(estadoPrestamo2));
+		assertTrue(estadoPrestamo2.equals(estadoPrestamo1));
+	}
+
+	@Test
+	public void testEqualsFalsoConEnDeuda() {
+		estadoPrestamo2 = new EnDeuda();
+		
+		assertFalse(estadoPrestamo1.equals(estadoPrestamo2));
+		assertFalse(estadoPrestamo2.equals(estadoPrestamo1));
+	}
+	
+	@Test
+	public void testEqualsFalsoConDeudorIncobrable() {
+		estadoPrestamo2 = new DeudorIncobrable();
+		
+		assertFalse(estadoPrestamo1.equals(estadoPrestamo2));
+		assertFalse(estadoPrestamo2.equals(estadoPrestamo1));
+	}
+	
+	@Test
+	public void testEqualsFalsoConFinalizado() {
+		estadoPrestamo2 = new Finalizado();
+		
+		assertFalse(estadoPrestamo1.equals(estadoPrestamo2));
+		assertFalse(estadoPrestamo2.equals(estadoPrestamo1));
+	}
+	
+	@Test
+	public void testEqualsFalsoConRechazado() {
+		estadoPrestamo2 = new Rechazado();
+		
+		assertFalse(estadoPrestamo1.equals(estadoPrestamo2));
+		assertFalse(estadoPrestamo2.equals(estadoPrestamo1));
+	}
+	
+	@Test
+	public void testEqualsFalsoConSolicitado() {
+		estadoPrestamo2 = new Solicitado();
+		
+		assertFalse(estadoPrestamo1.equals(estadoPrestamo2));
+		assertFalse(estadoPrestamo2.equals(estadoPrestamo1));
+	}
+		
+	@Test
+	public void testEqualsConElMismoEstado(){
+		assertTrue(estadoPrestamo1.equals(estadoPrestamo1));
+	}
+	
+	@Test
+	public void testEqualsConUnEstadoNull(){
+		assertFalse(estadoPrestamo1.equals(null));
+	}
 
 }
