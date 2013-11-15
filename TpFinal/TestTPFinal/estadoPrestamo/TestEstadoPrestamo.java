6441655916bb1de2451a54004f9af90ed8d8e7db
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-15 14:22:52 +0000

    se agrega el test equals al estadoPrestamo

---
+++ TpFinal/TestTPFinal/estadoPrestamo/TestEstadoPrestamo.java
@@ -0,0 +1,20 @@
+package estadoPrestamo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import estadoPrestamos.DeudorIncobrable;
+import estadoPrestamos.EnCurso;
+import estadoPrestamos.EnDeuda;
+import estadoPrestamos.EstadoPrestamo;
+import estadoPrestamos.Finalizado;
+import estadoPrestamos.Rechazado;
+import estadoPrestamos.Solicitado;
+
+public class TestEstadoPrestamo {
+
+	
+
+}
