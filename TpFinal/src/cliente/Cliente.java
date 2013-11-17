54f7a80d7f5dad03e5130226ae9759e61b0d9a10
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-17 14:44:52 +0000

    se agrego algo de codigo en cliente

--- TpFinal/src/cliente/Cliente.java
+++ TpFinal/src/cliente/Cliente.java
@@ -1,118 +1,24 @@
 package cliente;
 
-import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Vector;
 
-import estadoPrestamos.DeudorIncobrable;
-import estadoPrestamos.EnCurso;
-import estadoPrestamos.EnDeuda;
-import estadoPrestamos.Solicitado;
-import exceptions.AgregarPrestamoAClienteException;
 import prestamos.Prestamo;
 
-public class Cliente implements Observer {
+public abstract class Cliente implements Observer {
 	
-	private String apellido;
-	private String dni;
-	private String direccion;
-	private String nombre;
-	private List<Prestamo> prestamos;
+	public abstract boolean puedoAgregarPrestamo();
 	
-	
-	public List<Prestamo> getPrestamos() {
-		return prestamos;
-	}
-
-	public void setPrestamos(List<Prestamo> prestamos) {
-		this.prestamos = prestamos;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getApellido() {
-		return apellido;
-	}
-	
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	
-	public String getDni() {
-		return dni;
-	}
-	
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	
-	public String getDireccion() {
-		return direccion;
-	}
-	
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	/**
-	 * 
-	 * @param apellido
-	 * @param nombre
-	 * @param direccion
-	 * @param documento
-	 */
-	public Cliente(String apellido, String nombre, String direccion, String documento){
-		this.setApellido(apellido);
-		this.setNombre(nombre);
-		this.setDireccion(direccion);
-		this.setDni(documento);	
-		this.setPrestamos(new Vector<Prestamo>());
-	}
-	
-	public boolean puedoAgregarPrestamo()throws AgregarPrestamoAClienteException{
-		int contarPrestamos = 0;
-		for(Prestamo p : this.getPrestamos()){
-			if(contarPrestamos <= 2){
-				if(p.getEstado().equals(new EnCurso())){
-					contarPrestamos+=1;
-				}
-				if(p.getEstado().equals(new DeudorIncobrable()) || p.getEstado().equals(new Solicitado()) || p.getEstado().equals(new EnDeuda()) ) throw new AgregarPrestamoAClienteException("Usted no puede agregar prestamo");
-			}
-		}
-		if(contarPrestamos >= 2){
-			throw new AgregarPrestamoAClienteException("Usted no puede agregar prestamo");
-		}
-		return true;
-	}
-	
-	public void agregarPrestamo(Prestamo prestamo){
-		try{
-			if(this.puedoAgregarPrestamo()){
-				this.getPrestamos().add(prestamo);
-			}	
-		}
-		catch(AgregarPrestamoAClienteException e){
-			System.out.println(e.getMessage());
-		}
-	}
+	public abstract void agregarPrestamo(Prestamo prestamo);
 
 	@Override
-	public void update(Observable o, Object arg) {
-		System.out.println("Usted tiene un prestamo vencido");
-	}
+	public abstract void update(Observable o, Object arg);
 	
-	public void suscribirAlSistemaDeAviso(Prestamo prestamo){
-		prestamo.addObserver(this);
-	}
+	public abstract void suscribirAlSistemaDeAviso(Prestamo prestamo);
 	
-	public void salirSistemaDeAviso(Prestamo prestamo){
-		prestamo.deleteObserver(this);
-	}
+	public abstract void salirSistemaDeAviso(Prestamo prestamo);
 	
+	public abstract String obtenerDNI();
+	
+	public abstract String obtenerApellido();
 }
--- TpFinal/src/cliente/Persona.java
+++ TpFinal/src/cliente/Persona.java
@@ -1,5 +1,99 @@
 package cliente;
 
-public class Persona extends Cliente {
+import java.util.List;
+import java.util.Observable;
 
+import prestamos.Prestamo;
+
+public class Persona extends Cliente {
+	
+	private String apellido;
+	private String dni;
+	private String direccion;
+	private String nombre;
+	private List<Prestamo> prestamos;
+	
+	
+	public List<Prestamo> getPrestamos() {
+		return prestamos;
+	}
+
+	public void setPrestamos(List<Prestamo> prestamos) {
+		this.prestamos = prestamos;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+	
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	@Override
+	public boolean puedoAgregarPrestamo() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void agregarPrestamo(Prestamo prestamo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void suscribirAlSistemaDeAviso(Prestamo prestamo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void salirSistemaDeAviso(Prestamo prestamo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String obtenerDNI() {
+		return this.getDni();
+	}
+
+	@Override
+	public String obtenerApellido() {
+		return this.getApellido();
+	}
+
+	
+	
 }
