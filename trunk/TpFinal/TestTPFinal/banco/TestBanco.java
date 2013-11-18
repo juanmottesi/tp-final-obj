a0c893d1d25ce113ee3a94ba5ea828d53bbd249a
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-18 22:17:58 +0000

    se termino el test banco

--- trunk/TpFinal/TestTPFinal/banco/TestBanco.java
+++ trunk/TpFinal/TestTPFinal/banco/TestBanco.java
@@ -3,7 +3,7 @@ package banco;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import java.util.GregorianCalendar;
 
@@ -11,9 +11,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import prestamos.ConfiguracionPrestamo;
 import prestamos.Prestamo;
 import busqueda.Condicion;
 import cliente.Cliente;
+import exceptions.AprobadoException;
+import exceptions.EstadoClienteException;
+import exceptions.EstadoCuotaException;
+import exceptions.RechazadoException;
 
 public class TestBanco {
 
@@ -24,6 +29,7 @@ public class TestBanco {
 	ConfiguracionGeneral mockedConfiguracionGeneral = mock(ConfiguracionGeneral.class);
 	Condicion mockedCondicion = mock(Condicion.class);
 	Prestamo mockedPrestamo = mock(Prestamo.class);
+	ConfiguracionPrestamo mockedConfiguracionPrestamo = mock(ConfiguracionPrestamo.class);
 	
 	@Before 
 	public void setUp(){
@@ -99,6 +105,68 @@ public class TestBanco {
 		verify(mockedConfiguracionGeneral).finConfiguracionGeneral(fechaFin);
 	}
 	
+	@Test
+	public void testAceptarPrestamo() throws AprobadoException, EstadoClienteException{
+		banco.aceptarPrestamo(mockedPrestamo);
+		verify(mockedPrestamo).aceptarPrestamo();
+	}
 	
+	@Test
+	public void testRechazarPrestamo() throws RechazadoException, EstadoClienteException{
+		banco.rechazarPrestamo(mockedPrestamo);
+		verify(mockedPrestamo).rechazarPrestamo();
+	}
+	
+	 @Test
+	 public void testPagarCuota() throws EstadoCuotaException{
+		 GregorianCalendar hoy = new GregorianCalendar();
+		 banco.pagarCuota(mockedPrestamo,hoy);
+		 verify(mockedPrestamo).pagarCuota(hoy);
+	 }
+	 
+	 @Test
+	 public void testGenerarCuadroDeMarchaXMLDe(){
+		 banco.generarCuadroDeMarchaXMLDe(mockedPrestamo);
+		 verify(mockedPrestamo).genererarCuotasXML();
+	 }
+	 
+	 @Test
+	 public void testGenerarCuadroDeMarchaHTMLDe(){
+		 banco.generarCuadroDeMarchaHTMLDe(mockedPrestamo);
+		 verify(mockedPrestamo).genererarCuotasHTML();
+	 }
+	 
+	 @Test
+	 public void testAgregarPrestamoConClienteValido(){
+		when(mockedCliente.puedoAgregarPrestamo()).thenReturn(true);
+		
+		GregorianCalendar hoy = new GregorianCalendar();
+		GregorianCalendar fechaInicio = new GregorianCalendar(hoy.get(1),hoy.get(2),hoy.get(5)-1);
+		GregorianCalendar fechaFin = new GregorianCalendar(hoy.get(1)+100,hoy.get(2),hoy.get(5)-1);
+		
+		when(mockedConfiguracionGeneral.consultarTem(12)).thenReturn(3*0.01);
+		when(mockedConfiguracionGeneral.getFechaInicio()).thenReturn(fechaInicio);
+		when(mockedConfiguracionGeneral.getFechaFin()).thenReturn(fechaFin);
+		  
+		 banco.agregarPrestamo(25000, 12, new GregorianCalendar(), mockedCliente, mockedConfiguracionPrestamo);
+		 verify(mockedCliente).agregarPrestamo((Prestamo) any());
+		 assertEquals(1,banco.getPrestamos().size());
+	 }
+	 
+	 @Test
+	 public void testAgregarPrestamoConClienteNoValido(){
+		when(mockedCliente.puedoAgregarPrestamo()).thenReturn(false);
+		
+		GregorianCalendar hoy = new GregorianCalendar();
+		GregorianCalendar fechaInicio = new GregorianCalendar(hoy.get(1),hoy.get(2),hoy.get(5)-1);
+		GregorianCalendar fechaFin = new GregorianCalendar(hoy.get(1)+100,hoy.get(2),hoy.get(5)-1);
+		
+		when(mockedConfiguracionGeneral.consultarTem(12)).thenReturn(3*0.01);
+		when(mockedConfiguracionGeneral.getFechaInicio()).thenReturn(fechaInicio);
+		when(mockedConfiguracionGeneral.getFechaFin()).thenReturn(fechaFin);
+		  
+		 banco.agregarPrestamo(25000, 12, new GregorianCalendar(), mockedCliente, mockedConfiguracionPrestamo);
+		 assertEquals(0,banco.getPrestamos().size());
+	 }
 }
 
