f38b211d54f0f8800cd2898f873a46e51c5a476d
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-20 02:28:44 +0000

    se separa un poco el ejemplo para que quede mas claro que se hace en cada caso

--- trunk/TpFinal/src/Ejemplo/Ejemplo.java
+++ trunk/TpFinal/src/Ejemplo/Ejemplo.java
@@ -6,6 +6,7 @@ import java.util.Vector;
 
 import prestamos.ConfiguracionPrestamo;
 import prestamos.Prestamo;
+import seguro.MontoVariableReal;
 import seguro.SinSeguroDeVida;
 import cliente.Cliente;
 import cliente.Persona;
@@ -16,27 +17,41 @@ import banco.ConfiguracionGeneral;
 import gastos.Fijo;
 import gastos.Gasto;
 import gastos.GastoGlobal;
+import gastos.GastoMensual;
 
 public class Ejemplo {
 
 	
 	public static void main(String[] args) {
+		//Creo dos fechas
 		GregorianCalendar fechaHoy = new GregorianCalendar();
 		GregorianCalendar fechaDeCreacionBanco = new GregorianCalendar(fechaHoy.get(1), fechaHoy.get(2), fechaHoy.get(5)-1);
+		//Creo el banco
 		Banco banco = new Banco();
+		//Creo una configuracion General
 		List<Gasto> gastosConfiguracionGeneral = new Vector<Gasto>();
 		gastosConfiguracionGeneral.add(new GastoGlobal(new Fijo(3)));
 		ConfiguracionGeneral configuracionGeneral = new ConfiguracionGeneral(fechaDeCreacionBanco, gastosConfiguracionGeneral);
 		configuracionGeneral.agregarClaveValorATem(12, 3);
+		//Agrego al banco la configuracion general
 		banco.agregarConfiguracionGeneral(configuracionGeneral);
+		//Creo un cliente
 		Persona primerCliente = new Persona("apellido","nombre","36778000","direccion");
-		ConfiguracionPrestamo configuracionPrestamo = new ConfiguracionPrestamo(new SinSeguroDeVida(), gastosConfiguracionGeneral);
+		//Creo la configuracion prestamo
+		List<Gasto> gastosConfiguracionPrestamo = new Vector<Gasto>();
+		gastosConfiguracionPrestamo.add(new GastoMensual(new Fijo(3)));
+		ConfiguracionPrestamo configuracionPrestamo = new ConfiguracionPrestamo(new MontoVariableReal(2), gastosConfiguracionPrestamo);
+		//Agrego un prestamo al banco y al cliente
 		banco.agregarPrestamo(25000, 12, fechaHoy, primerCliente, configuracionPrestamo);
-		
+		//me guardo el prestamo en una variable auxiliar
 		Prestamo primerPrestamo = banco.getPrestamos().get(0);
+		//aseptamos el prestamo 
 		banco.aceptarPrestamo(primerPrestamo);
-		banco.pagarCuota(primerPrestamo, fechaHoy);
+		//generamos el cuadro de marcha html
 		banco.generarCuadroDeMarchaHTMLDe(primerPrestamo);
+		// pagamos una cuota del prestamo
+		banco.pagarCuota(primerPrestamo, fechaHoy);
+		//generamos el cuadro de marcha xml
 		banco.generarCuadroDeMarchaXMLDe(primerPrestamo);
 	}
 }
