b3623e5a062a725f6deb120e2f8669d3c4e6a6bb
juannpm159@gmail.com <juannpm159@gmail.com@68b71c74-c1e2-5d88-05a8-69a5b21eabb6>
2013-11-21 14:23:37 +0000

    se termino lo de los html y xml

--- TpFinal/TestTPFinal/tipoDeImpresion/TestPorHTML.java
+++ TpFinal/TestTPFinal/tipoDeImpresion/TestPorHTML.java
@@ -4,12 +4,15 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Vector;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
 import prestamos.Cuota;
+import prestamos.Prestamo;
 
 public class TestPorHTML {
 	
@@ -17,6 +20,7 @@ public class TestPorHTML {
 
 	@Mock
 	Cuota mockedCuota = mock(Cuota.class);
+	Prestamo mockedPrestamo = mock(Prestamo.class);
 	
 	@Before
 	public void setUp(){
@@ -221,5 +225,94 @@ public class TestPorHTML {
 	@Test
 	public void testNombreArchivo(){
 		assertEquals("Prestamo.html", porHTML.nombreArchivo());
+	}
+	
+	@Test
+	public void testImrimir(){
+		String texto = "";
+		texto = texto + porHTML.generarCabecera();
+		texto = texto + this.generarTextoParaTestGenerarCuerpo();
+		texto = texto + porHTML.generarFinCabecera();
+		
+		assertEquals(texto, porHTML.imprimir(mockedPrestamo));
+	}
+	
+	public String generarTextoParaTestGenerarCuerpo(){
+		List<Cuota> cuotas = new Vector<Cuota>();
+		cuotas.add(mockedCuota);
+		when(mockedPrestamo.getCuotas()).thenReturn(cuotas);
+		String texto = "";
+		texto = texto + porHTML.generarCuerpo();
+		texto = texto + this.generarTextoParaTestGenerarInfo();
+		texto = texto + porHTML.generarFinCuerpo();
+		return texto;
+	}
+	
+	@Test
+	public void testGenerarCurpo(){
+		
+		String texto = this.generarTextoParaTestGenerarCuerpo();
+		assertEquals(texto, porHTML.generarCuerpo(mockedPrestamo));
+	}
+	
+	public String generarTextoParaTestGenerarInfo(){
+		when(mockedCuota.getNroCuota()).thenReturn(1);
+		when(mockedCuota.mostrarFecha(mockedCuota.getFechaVencimiento())).thenReturn("fecha");
+		when(mockedCuota.getAmortizacion()).thenReturn((double) 1);
+		when(mockedCuota.getInteres()).thenReturn((double) 1);
+		when(mockedCuota.getSaldoDeuda()).thenReturn((double) 1);
+		when(mockedCuota.getSeguro()).thenReturn((double) 1);
+		when(mockedCuota.getGastoTotal()).thenReturn((double) 1);
+		when(mockedCuota.getMontoCuota()).thenReturn((double) 1);
+		when(mockedCuota.getValorTotalCuota()).thenReturn((double) 1);
+		when(mockedCuota.mostrarFecha(mockedCuota.getFechaDePago())).thenReturn("fecha");
+		when(mockedCuota.getInteresPorMora()).thenReturn((double) 1);
+		
+		String nuevalinea = System.getProperty("line.separator");
+		String nuevalinea2 = nuevalinea+"            ";
+		String s = "";
+
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Cuota " + 1;
+		s = s+"</li>";
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Vencimiento " + "fecha";
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Amortizacion " + 1.00;
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Interes " + 1.00;
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Saldo Deuda " + 1.00;
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Seguro " + 1.00;
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Gastos " + 1.00;
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Valor Cuota " + 1.00;
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Valor Total Cuota " + 1.00;
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Fecha de Pago " + "fecha";
+		s = s+"</li>";	
+		s = s+ nuevalinea2+"<li>";
+		s = s + "Interes Por Mora " + 1.00;
+		s = s+"</li>";	
+		
+		return s;
+	}
+	
+	@Test
+	public void testGenerarInfo(){
+		String s = this.generarTextoParaTestGenerarInfo();
+		assertEquals(s, porHTML.generarInfo(mockedCuota));
+		
 	}
 }
